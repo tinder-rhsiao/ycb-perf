074839dc92ebbecae0996e97c5b94b3f3f332c8b
Roderick Hsiao <roderick.hsiao@gotinder.com>
2017-02-14 17:24:44 -0800

    add result

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## Perf Results
+
+```console
+YCB x 158,928 ops/sec ±1.48% (85 runs sampled)
+YCB Multiple x 119,293 ops/sec ±1.29% (85 runs sampled)
+Object assign x 871,120 ops/sec ±1.11% (87 runs sampled)
+Object assign multiple x 437,280 ops/sec ±1.23% (85 runs sampled)
+Fastest is Object assign
+```
--- tests/index.js
+++ tests/index.js
@@ -2,16 +2,24 @@ import Benchmark from 'benchmark';
 
 import YCB from 'ycb';
 import dimensions from './fixtures/dimensions.json';
-import configs from './fixtures/dimensions.json';
+import configs from './fixtures/sample.json';
+import configsMultiple from './fixtures/sample-multiple.json';
 
 const suite = new Benchmark.Suite;
-const ycbObj = new YCB.Ycb([dimensions].concat(configs));
+const ycbObj = new YCB.Ycb(dimensions.concat(configs));
+const ycbMultiple = new YCB.Ycb(dimensions.concat(configsMultiple));
 
 suite.add('YCB', () => {
-  ycbObj.read();
+  ycbObj.read({});
+})
+suite.add('YCB Multiple', () => {
+  ycbMultiple.read({ lang: 'fr' });
 })
 suite.add('Object assign', () => {
   Object.assign({}, dimensions[0], configs[0])
+})
+suite.add('Object assign multiple', () => {
+  Object.assign({}, dimensions[0], configs[0], configsMultiple[0], configsMultiple[1], configsMultiple[2]);
 })
 .on('cycle', function(event) {
   console.log(String(event.target));
